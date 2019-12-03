282306c11ae4c50aa03ec842b5946a57ccf9ce4e
Floofy.KH <floofy.kh@gmail.com>
2019-12-03 21:27:19 +0000

    Begun adding unit tests to C# bindings.

---
+++ dialogue_editor/DialogueManagerTests/DialogueManagerTest.cs
@@ -0,0 +1,44 @@
+﻿using floofy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DialogueManagerTests
+{
+    [TestClass]
+    public class DialogueManagerTest
+    {
+        [TestMethod]
+        public void ManagerBeginsWithZeroDialogues()
+        {
+            var mgr = new DialogueManager();
+
+            Assert.AreEqual(mgr.NumDialogues, 0);
+        }
+
+        [TestMethod]
+        public void AddingDialoguesIncreasesDialogueCount()
+        {
+            var mgr = new DialogueManager();
+
+            mgr.AddDialogue("New Dialogue 1");
+            Assert.AreEqual(mgr.NumDialogues, 1);
+
+            mgr.AddDialogue("New Dialogue 2");
+            Assert.AreEqual(mgr.NumDialogues, 2);
+        }
+
+        [TestMethod]
+        public void RemovingDialoguesDecreasesDialogueCount()
+        {
+            var mgr = new DialogueManager();
+
+            mgr.AddDialogue("New Dialogue 1");
+            mgr.AddDialogue("New Dialogue 2");
+
+            mgr.RemoveDialogue("New Dialogue 1");
+            Assert.AreEqual(mgr.NumDialogues, 1);
+
+            mgr.RemoveDialogue("New Dialogue 2");
+            Assert.AreEqual(mgr.NumDialogues, 0);
+        }
+    }
+}
---
+++ dialogue_editor/DialogueManagerTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DialogueManagerTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DialogueManagerTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8b571cbe-53df-4ee2-a0a8-cf4ae8c4e9a0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
